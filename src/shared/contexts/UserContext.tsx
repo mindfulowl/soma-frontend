ff662b588233f5ba8b4b8752d2677116323423be
Joshua Da Costa <joshuadacosta0311@gmail.com>
2023-06-01 14:49:50 +0100

    update

--- src/shared/contexts/UserContext.tsx
+++ src/shared/contexts/UserContext.tsx
@@ -14,8 +14,8 @@ import {
 import jwt_decode from "jwt-decode";
 
 export const amplifyConfig = Amplify.configure({
-  aws_cognito_region: process.env.REACT_APP_AWS_REGION,
-  aws_user_pools_id: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+  aws_cognito_region: "eu-west-2",
+  aws_user_pools_id: "eu-west-2_siv7j3Ap4",
   aws_user_pools_web_client_id: process.env.REACT_APP_COGNITO_CLIENT_ID,
   aws_mandatory_sign_in: "enable",
   oauth: {
